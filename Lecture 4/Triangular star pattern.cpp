8e3a205a05ac684364b878843638c8a364732d1f
Manish <73701565+MG1902@users.noreply.github.com>
2022-07-09 13:19:01 +0530

    Create Triangular star pattern.cpp

---
+++ Lecture 4/Triangular star pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i;j<=2*i-1;j++)
+            cout<<"*";
+        cout<<"\n";
+    }
+  
+}
+
+
