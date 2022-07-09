3a9a9443b42ab6e28b7e62eaa79daf80b46f8b15
Manish <73701565+MG1902@users.noreply.github.com>
2022-07-09 13:06:48 +0530

    Update Number Pattern 2.cpp

--- Lecture 2/Number Pattern 2.cpp
+++ Lecture 2/Number Pattern 2.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=i;k<n;k++)
+            cout<<" ";
+        for(int j=i;j<2*i;j++)
+            cout<<j;
+            cout<<"\n";
+    }
+}
+
+
