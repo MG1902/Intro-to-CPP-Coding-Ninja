e5aafd27287c0ab9e64a94f029ca5a10e7fc50b3
Manish <73701565+MG1902@users.noreply.github.com>
2022-07-09 13:34:27 +0530

    Create Square Root (Integral).cpp

---
+++ lecture 6/Square Root (Integral).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+    cin>>n;
+    if(n==0)
+        cout<<"0";
+    else
+    cout<<int(pow(n,0.5));
+}
